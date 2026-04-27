661364e6af9c835ebaaa3b5dde1d58cf07fae087
Miodrag Djukic <miotronic@gmail.com>
2026-04-28 00:49:05 +0400

    Update start.sh
    Add web server, fix KMS/DRM wait, fix paths

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Wait for KMS/DRM device to be ready
+python3 /home/zero/teleprompter/webserver.py >> /home/zero/teleprompter/webserver.log 2>&1 &
 while [ ! -e /dev/dri/card0 ]; do sleep 1; done
 while ! SDL_VIDEODRIVER=kmsdrm SDL_AUDIODRIVER=dummy SDL_VIDEO_KMSDRM_DEVICE=/dev/dri/card0 python3 -c "import pygame; pygame.display.init(); pygame.display.set_mode((800,480), pygame.FULLSCREEN); pygame.quit()" 2>/dev/null; do
     sleep 2
 done
-
 export SDL_VIDEODRIVER=kmsdrm
 export SDL_AUDIODRIVER=dummy
 export SDL_VIDEO_KMSDRM_DEVICE=/dev/dri/card0
